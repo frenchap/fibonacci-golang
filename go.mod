029c4aceb1f6fda19906a2fc5bc097344439dcd3
Andy French <afrench@securesky.com>
2021-06-16 22:04:56 -0400

    initial commit

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/frenchap/fibonacci-golang
 go 1.16
 
 require (
-	github.com/lib/pq v1.10.2
 	github.com/ory/dockertest/v3 v3.6.5
 	github.com/sirupsen/logrus v1.8.1
 )
--- test/fibonacci_test.go
+++ test/fibonacci_test.go
@@ -1,14 +1,67 @@
 package test
 
 import (
+	"fmt"
 	"testing"
-
-	"gitlab.com/frenchap/fibonacci-golang/fibonacciGenerator"
+	"time"
 )
+
+type FibonacciElement struct {
+	Y        int
+	TimeCost time.Duration
+}
+
+type IFibonacciStore interface {
+	GetMax() int
+	GetValue(x int) (FibonacciElement, error)
+}
+
+type MemoryFibonacciStore struct {
+	UpperBound int
+	Sequence   map[int]FibonacciElement
+}
+
+func NewMemoryFibonacciStore(upperBound int) MemoryFibonacciStore {
+	return MemoryFibonacciStore{
+		UpperBound: upperBound,
+		Sequence: map[int]FibonacciElement{
+			0: {0, 0},
+			1: {1, 0},
+		},
+	}
+}
+
+func (thisStore MemoryFibonacciStore) GetMax() int {
+	return len(thisStore.Sequence) - 1
+}
+
+func (thisStore MemoryFibonacciStore) GetValue(x int) (FibonacciElement, error) {
+	if value, valueFound := thisStore.Sequence[x]; valueFound {
+		return value, nil
+	}
+	if x > thisStore.UpperBound {
+		return FibonacciElement{0, 0}, fmt.Errorf("requested value of %d above upper bound of %d", x, thisStore.UpperBound)
+	}
+
+	start := time.Now()
+	for i := 2; i <= x; i++ {
+		thisStore.Sequence[i] = FibonacciElement{thisStore.Sequence[i-2].Y + thisStore.Sequence[i-1].Y, time.Since(start)}
+	}
+
+	returnThis := thisStore.Sequence[x]
+	return returnThis, nil
+}
+
+type FibonacciGenerator struct {
+}
+
+func (thisGenerator FibonacciGenerator) CalculateFibonacci(x int) int {
+	return 0
+}
 
 func TestFibonacciMemoryStore(t *testing.T) {
 	testUpperBound := 1000
-	var testStore fibonacciGenerator.IFibonacciStore = fibonacciGenerator.NewMemoryFibonacciStore(testUpperBound)
+	var testStore IFibonacciStore = NewMemoryFibonacciStore(testUpperBound)
 
 	expectedMax := 1
 
